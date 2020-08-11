d1cadff294c830ffb5333861a8aa11c6618c8fad
Priscila F. Nascimento <Priscila F. Nascimento>
2020-08-11 18:23:22 -0300

    Exercício Dictionary

---
+++ ExercicioDictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExercicioDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            Dictionary<string, int> candidate = new Dictionary<string, int>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] candidateInfos = sr.ReadLine().Split(',');
+                    string name = candidateInfos[0];
+                    int votes = int.Parse(candidateInfos[1]);
+
+                    if (candidate.ContainsKey(name))
+                    {
+                        candidate[name] += votes;
+                    }
+                    else
+                    {
+                        candidate[name] = votes;
+                    }
+
+                }
+                foreach (var item in candidate)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+
+            }
+        }
+    }
+}
